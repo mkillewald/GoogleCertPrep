ec961d85110b8d84f182151fdcabcbb886518c87
mkillewald <mkillewald@yahoo.com>
2018-11-18 10:10:21 -0500

    first commit

---
+++ CustomEditText/app/src/main/java/com/gameaholix/customedittext/MainActivity.java
@@ -0,0 +1,16 @@
+package com.gameaholix.customedittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
